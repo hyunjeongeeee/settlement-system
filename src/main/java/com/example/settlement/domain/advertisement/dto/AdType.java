44a43b6e751a543e3f9b466b787ddc3ee6614192
hyunjeong <hyunjeong9592@gmail.com>
2024-10-20 21:14:54 +0900

    🔥 중복 파일 삭제

--- src/main/java/com/example/settlement/domain/advertisement/dto/AdType.java
+++
@@ -1,5 +0,0 @@
-package com.example.settlement.domain.advertisement.dto;
-
-public enum AdType {
-    SKIP, NONSKIP
-}
--- src/main/java/com/example/settlement/domain/user/dto/SocialType.java
+++
@@ -1,5 +0,0 @@
-package com.example.settlement.domain.user.dto;
-
-public enum SocialType {
-    KAKAO, NAVER, GOOGLE
-}
