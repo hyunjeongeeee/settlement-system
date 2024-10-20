b60a343312e479139d81b612124d40feeebdeab7
hyunjeong <hyunjeong9592@gmail.com>
2024-10-20 21:25:25 +0900

    🔧 SettlementApplication 실행파일 수정 DataSourceAutoConfiguration.class 삭제

--- src/main/java/com/example/settlement/SettlementApplication.java
+++ src/main/java/com/example/settlement/SettlementApplication.java
@@ -2,9 +2,8 @@ package com.example.settlement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class SettlementApplication {
 
 	public static void main(String[] args) {
